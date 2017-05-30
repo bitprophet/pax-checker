751221e82c7a1ad946f7bb9a7abb08bfed2d7f79
Jeff Forcier <jeff@bitprophet.org>
2017-05-30 12:54:41 -0700

    Twitter integration seems to work...?!

--- tasks.py
+++ tasks.py
@@ -1,17 +1,53 @@
 from invoke import task
 import pypd
+import tweepy
 
 
 @task(name='check-loop')
 def check_loop(c):
-    # This is how to configure pypd. Meh.
-    pypd.api_key = c.pagerduty.api_key
     # TODO:
     # - check PAX public twitter feed
     # - loop
     # - when any public tweet:
-    # - send event as below
+    # - send page
     # - link to the tweet itself probably
+    pass
+
+
+def twitter_auth(c):
+    auth = tweepy.OAuthHandler(
+        c.twitter.consumer_key,
+        c.twitter.consumer_secret,
+    )
+    auth.set_access_token(
+        c.twitter.access_token,
+        c.twitter.access_secret,
+    )
+    return auth
+
+
+class Anxious(tweepy.StreamListener):
+    def on_status(self, status):
+        # Only alert on real tweets, not replies, PAX acct definitely replies
+        # to @-messages sometimes and I don't want to get paged on those!
+        if not status.in_reply_to_status_id:
+            print("Status: {}".format(status))
+
+    def on_error(self, status_code):
+        print("Error {}!".format(status_code))
+
+
+@task(name='stream-tweets')
+def stream_tweets(c):
+    auth = twitter_auth(c)
+    stream = tweepy.Stream(auth=auth, listener=Anxious())
+    stream.filter(follow=[str(c.twitter.follow_id)])
+
+
+@task(name='send-page')
+def send_page(c, url):
+    # This is how to configure pypd. Mehhhhh
+    pypd.api_key = c.pagerduty.api_key
     pypd.Event.create(data={
         'service_key': c.pagerduty.service_key,
         'event_type': 'trigger',
@@ -19,8 +55,8 @@ def check_loop(c):
         'contexts': [
               {
                   'type': 'link',
-                  'href': 'http://west.paxsite.com/registration',
-                  'text': 'OMGOMGOMG',
+                  'href': url,
+                  'text': 'Link to tweet!',
               },
         ],
     })
